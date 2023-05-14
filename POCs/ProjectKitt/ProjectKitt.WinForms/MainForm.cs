11bc962f7ec1348e1b8761ee9422ba8d4069dbf9
Brent Acs <brent.mann@gmail.com>
2023-05-14 08:11:27 -0400

    very simple move and rotation POC

--- POCs/ProjectKitt/ProjectKitt.WinForms/MainForm.cs
+++ POCs/ProjectKitt/ProjectKitt.WinForms/MainForm.cs
@@ -7,7 +7,7 @@ namespace ProjectKitt.WinForms;
 public partial class MainForm : Form
 {
    private PointF _location = PointF.Empty;
-   private float _heading = 45f;
+   private float _heading = 0f;
 
    public MainForm()
    {
@@ -18,8 +18,6 @@ public partial class MainForm : Form
 
    private void MainForm_Paint(object sender, PaintEventArgs e)
    {
-      System.Diagnostics.Debug.WriteLine($"location: {_location}");
-
       var painter = new ViewPortPainter(ClientRectangle);
       painter.TestPaint(e.Graphics, _location, _heading);
    }
@@ -32,7 +30,8 @@ public partial class MainForm : Form
 
    private void button1_Click(object sender, EventArgs e)
    {
-      _location = ComputeNew(_location, 45, 10);
+      _location = ComputeNew(_location, _heading, 25);
+      _heading += 15;
       Invalidate();
    }
 }
@@ -74,22 +73,45 @@ public class ViewPortPainter
       //g.DrawRectangle(pen, rect);
 
 
-      var radians = ((double)heading).ToRadians();
+      //var radians = ((double)heading).ToRadians();
       //return new PointF((float)(starting.X + distance * Math.Sin(radians)), (float)(starting.Y + distance * Math.Cos(radians)));
 
-      var path = new GraphicsPath();
-      
-
-
-
-      var rect = new RectangleF(
-         display.X - 4,
-         display.Y - 12, 
-         8, 
-         24);
-      //g.DrawPath();
+      var points = new List<PointF>
+      {
+         new(display.X - 4, display.Y - 12),
+         new(display.X + 4, display.Y - 12),
+         new(display.X + 4, display.Y + 12),
+         new(display.X - 4, display.Y + 12),
+      };
+      var rotated = RotatePolygon(points, display, heading);
+      ClosePolygon(rotated);
+      g.DrawPolygon(pen, rotated.ToArray());
    }
 
+   private static IList<PointF> RotatePolygon(ICollection<PointF> polygon, PointF center, float angle)
+   {
+      var radians = ((double)angle).ToRadians();
+      var rotated = new List<PointF>();
+      foreach (var point in polygon)
+      {
+         var adjusted = new PointF(
+            (float) (Math.Cos(radians) * (point.X - center.X) - Math.Sin(radians) * (point.Y-center.Y) + center.X),      
+            (float) (Math.Sin(radians) * (point.X - center.X) + Math.Cos(radians) * (point.Y-center.Y) + center.Y)      
+            );
+         rotated.Add(adjusted);
+      }
+      
+      return rotated;
+   }
+
+   private static void ClosePolygon(ICollection<PointF> polygon)
+   {
+      var first = polygon.First();
+      var last = polygon.Last();
+      if( first != last)
+         polygon.Add(first);
+   }
+   
    private PointF GetViewCenter()
    {
       return new PointF(ViewRect.Width / 2, ViewRect.Height / 2);
