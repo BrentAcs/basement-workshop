77533eb06f56b9bc8b059d8c0db3713656dcd2a3
Brent Acs <brent.mann@gmail.com>
2023-05-14 07:20:27 -0400

    WIP

--- POCs/ProjectKitt/ProjectKitt.WinForms/MainForm.cs
+++ POCs/ProjectKitt/ProjectKitt.WinForms/MainForm.cs
@@ -1,9 +1,105 @@
-﻿namespace ProjectKitt.WinForms;
+﻿using System.Net.NetworkInformation;
+using Bass.Shared.Extensions;
+
+namespace ProjectKitt.WinForms;
 
 public partial class MainForm : Form
 {
+   private PointF _location = PointF.Empty;
+
    public MainForm()
    {
       InitializeComponent();
+   }
+
+   private void MainForm_SizeChanged(object sender, EventArgs e) => Invalidate();
+
+   private void MainForm_Paint(object sender, PaintEventArgs e)
+   {
+      System.Diagnostics.Debug.WriteLine($"location: {_location}");
+
+      var painter = new ViewPortPainter(ClientRectangle);
+      painter.TestPaint(e.Graphics, _location);
+   }
+
+   static PointF ComputeNew(PointF starting, float heading, float distance)
+   {
+      var radians = ((double)heading).ToRadians();
+      return new PointF((float)(starting.X + distance * Math.Sin(radians)), (float)(starting.Y + distance * Math.Cos(radians)));
+   }
+
+   private void button1_Click(object sender, EventArgs e)
+   {
+      _location = ComputeNew(_location, 45, 10);
+      Invalidate();
+   }
+}
+
+public class ViewPortPainter
+{
+   public ViewPortPainter(RectangleF viewRect)
+   {
+      ViewRect = viewRect;
+   }
+
+   public ViewPortPainterOptions Options { get; set; } = new();
+   public RectangleF ViewRect { get; set; }
+   public PointF ViewCenter => GetViewCenter();
+
+   private void DrawAxis(Graphics g)
+   {
+      if (!Options.Axis.Show)
+         return;
+
+      using var pen = new Pen(Options.Axis.Color, Options.Axis.Width);
+      g.DrawLine(pen, ViewRect.Width / 2, 0, ViewRect.Width / 2, ViewRect.Height);
+      g.DrawLine(pen, 0, ViewRect.Height / 2, ViewRect.Width, ViewRect.Height / 2);
+   }
+
+   public void TestPaint(Graphics g, PointF point, float heading)
+   {
+      DrawAxis(g);
+
+      var display = new PointF(ViewCenter.X + point.X, ViewCenter.Y - point.Y);
+      using var pen = new Pen(Color.Red);
+      //g.DrawEllipse(pen, display.X - 2, display.Y - 2, 4, 4);
+
+      //var rect = new RectangleF(
+      //   display.X - 4,
+      //   display.Y - 12,
+      //   8,
+      //   24);
+      //g.DrawRectangle(pen, rect);
+
+
+      var radians = ((double)heading).ToRadians();
+      //return new PointF((float)(starting.X + distance * Math.Sin(radians)), (float)(starting.Y + distance * Math.Cos(radians)));
+
+
+
+
+      var rect = new RectangleF(
+         display.X - 4,
+         display.Y - 12, 
+         8, 
+         24);
+      //g.DrawPath();
+   }
+
+   private PointF GetViewCenter()
+   {
+      return new PointF(ViewRect.Width / 2, ViewRect.Height / 2);
+   }
+}
+
+public class ViewPortPainterOptions
+{
+   public AxisOptions Axis { get; set; } = new();
+
+   public class AxisOptions
+   {
+      public bool Show { get; set; } = true;
+      public Color Color { get; set; } = Color.Black;
+      public int Width { get; set; } = 2;
    }
 }
--- POCs/ProjectKitt/ProjectKitt.WinForms/Program.cs
+++ POCs/ProjectKitt/ProjectKitt.WinForms/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows.Forms.VisualStyles;
-using Bass.Shared.Extensions;
+﻿using Bass.Shared.Extensions;
 
 namespace ProjectKitt.WinForms;
 
@@ -13,25 +12,25 @@ internal static class Program
    {
       // To customize application configuration such as set high DPI settings or default font,
       // see https://aka.ms/applicationconfiguration.
-      //ApplicationConfiguration.Initialize();
-      //Application.Run(new MainForm());
+      ApplicationConfiguration.Initialize();
+      Application.Run(new MainForm());
 
       //var heading = 0f;
-      var starting = PointF.Empty;
-      var distance = 10f;
+      //var starting = PointF.Empty;
+      //var distance = 10f;
 
-      // var headings = new[] { 0f, 45f, 90f, 180f, 270f, 360f };
-      // foreach (float heading in headings)
-      // {
-      //    var ending = ComputeNew(starting, heading, 10);
-      //    System.Diagnostics.Debug.WriteLine($"heading: {heading} -> {ending}");
-      // }
+      //// var headings = new[] { 0f, 45f, 90f, 180f, 270f, 360f };
+      //// foreach (float heading in headings)
+      //// {
+      ////    var ending = ComputeNew(starting, heading, 10);
+      ////    System.Diagnostics.Debug.WriteLine($"heading: {heading} -> {ending} ");
+      //// }
 
-      for (float heading = 0; heading < 360; heading += 45)
-      {
-         var ending = ComputeNew(starting, heading, 10);
-         // System.Diagnostics.Debug.WriteLine($"heading: {heading} -> {ending}");
-      }
+      //for (float heading = 0; heading < 91; heading += 15)
+      //{
+      //   var ending = ComputeNew(starting, heading, 10);
+      //   // System.Diagnostics.Debug.WriteLine($"heading: {heading} -> {ending}");
+      //}
 
       //x = r cosθ and y = r sinθ
    }
