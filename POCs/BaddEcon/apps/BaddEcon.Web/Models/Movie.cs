d13a8457652a3eaa3e37a4d2637492a4dd341412
Brent Acs <brent.mann@gmail.com>
2023-08-04 21:53:03 -0400

    WIP

---
+++ POCs/BaddEcon/apps/BaddEcon.Web/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaddEcon.Web.Models;
+
+// https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-model?view=aspnetcore-7.0&tabs=visual-studio
+public class Movie
+{
+   public int Id { get; set; }
+   public string? Title { get; set; }
+   [DataType(DataType.Date)]
+   public DateTime ReleaseDate { get; set; }
+   public string? Genre { get; set; }
+   public decimal Price { get; set; }
+}
