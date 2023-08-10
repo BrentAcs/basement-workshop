6b30e6dd69744f36c7682752d357c87c794a7a60
Brent Acs <brent.mann@gmail.com>
2023-08-10 07:49:49 -0400

    WIP

--- POCs/Turncoats/apps/Turncoats.WebUI/Program.cs
+++ POCs/Turncoats/apps/Turncoats.WebUI/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Turncoats.Blazor;
+using Turncoats.WebUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +9,3 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
 await builder.Build().RunAsync();
-
-// ref:  https://www.codeproject.com/Articles/5269947/Drawing-with-the-HTML-Canvas-Element-in-Blazor-Ser
